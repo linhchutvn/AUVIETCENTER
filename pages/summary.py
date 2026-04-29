1e1486dcae768b8672dd44dd2a27a46c6689fb2d
linhchutvn <linhnts4t@gmail.com>
2026-04-29 16:38:18 +0700

    Update summary.py

--- pages/summary.py
+++ pages/summary.py
@@ -59,7 +59,7 @@ def generate_content_with_failover(prompt, image=None, json_mode=False):
     keys_to_try = list(ALL_KEYS)
     random.shuffle(keys_to_try) 
     model_priority = [
-        #"gemini-3-flash-preview",        
+        "gemini-3.1-pro-preview",        
         #"gemini-2.5-pro",
         "gemini-2.5-flash",
         "gemini-2.5-flash-lite",
